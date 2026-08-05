eddfb3dfe87c4ee310116408c07cbb94c3f30fa9
J. Terrazas <6402198+terrator@users.noreply.github.com>
2026-08-05 09:38:35 -0400

    - fixed latest issues

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { IonicModule } from '@ionic/angular';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    imports: [RouterTestingModule, HttpClientTestingModule, TranslateModule.forRoot(), IonicModule.forRoot()],
+    declarations: [AppComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
   }));
 
   it('should create the app', () => {
@@ -24,6 +29,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('translator-demo app is running!');
+    expect(compiled.querySelector('ion-title')).toBeTruthy();
   });
 });
--- src/app/translate-page/translate-page.component.spec.ts
+++ src/app/translate-page/translate-page.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+import { IonicModule } from '@ionic/angular';
 
 import { TranslatePageComponent } from './translate-page.component';
 
@@ -8,6 +12,7 @@ describe('TranslatePageComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, TranslateModule.forRoot(), IonicModule.forRoot(), FormsModule],
       declarations: [TranslatePageComponent]
     });
     fixture = TestBed.createComponent(TranslatePageComponent);
